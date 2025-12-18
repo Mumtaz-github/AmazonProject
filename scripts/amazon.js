1dbf6c62acf9386a0c90820dc81a248ffd9c2676
Mumtaz-github <mumtazfra@gmail.com>
2025-12-18 16:54:39 +0100

    save

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,35 +1,5 @@
 
-//because array represent list
-//saved image inside js
-const products = [{
-image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-name:  'Black and Gray Athletic Cotton Socks - 6 Pairs',
-rating: {
-    stars: 4.5,
-    count: 87
-},
-//save price in cents because js floating issue for money use cen
-priceCents: 1090
-//here another object for another product
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: ' Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: ' Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count:   56
-    },
-    priceCents: 799
-}];
-//to combine together we create a variable at the top for 
-//combining all html the strings together
+
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -80,16 +50,25 @@ productsHTML  += `
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button 
+          button-primary js-add-to-cart"
+          data-product-name="${product.name}">
             Add to Cart
           </button>
         </div>
         `;
        
  });
- console.log(productsHTML);
+//  console.log(productsHTML);
 
 
  //use dom
  document.querySelector('.js-products-grid').
  innerHTML = productsHTML;
+
+ document.querySelectorAll('.js-add-to-cart')
+ .forEach((button) => {
+    button.addEventListener('click', () => {
+      
+    });
+ });
