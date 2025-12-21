7013ad3fc876a8a81e2a4c48d87b719be03ef8f1
Mumtaz-github <mumtazfra@gmail.com>
2025-12-21 01:37:13 +0100

    import products from product.js

--- data/products.js
+++ data/products.js
@@ -1,4 +1,5 @@
-const products = [
+  //i export to the amazon.js
+  export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
