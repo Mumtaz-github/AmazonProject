cd46eba691dbbabca3f37a3a61b1189534c9c02a
Mumtaz-github <mumtazfra@gmail.com>
2025-12-21 01:41:31 +0100

    save

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -69,6 +69,9 @@ productsHTML  += `
  document.querySelector('.js-products-grid').
  innerHTML = productsHTML;
 
+
+ 
+
  document.querySelectorAll('.js-add-to-cart')
  .forEach((button) => {
     button.addEventListener('click', () => {
